b7df4459e3bd644f035aa359e0861184b7aff965
shankarranganathan <shankarmech100@gmail.com>
2024-09-17 10:10:45 +0530

    shancommit

--- src/org/demos/Abc.java
+++ src/org/demos/Abc.java
@@ -3,6 +3,9 @@ package org.demos;
 public class Abc {
 	public static void main(String[] args) {
 		System.out.println("123");
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
 	}
 
 }
